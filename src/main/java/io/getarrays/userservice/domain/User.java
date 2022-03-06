ebabe028f984c3821583eb0acd77d0d7b123ba39
mahdinezhadasad <mahdinezhadasad@gmail.com>
2022-03-06 18:39:29 +0100

    First JWT Project

---
+++ src/main/java/io/getarrays/userservice/domain/User.java
@@ -0,0 +1,25 @@
+package io.getarrays.userservice.domain;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+
+
+}
